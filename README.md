f32e60e83b27cbfe05ec98597c193a9c9bd664e1
David Charte <david@deivi.ch>
2022-06-24 19:47:12 +0200

    Mejora animaciones

---
+++ README.md
@@ -0,0 +1,20 @@
+# defensa
+
+Diapositivas para la defensa de tesis de [@fdavidcl](https://github.com/fdavidcl).
+
+## Uso
+
+Puedes acceder a la versión actual de las diapositivas en [deivi.ch/defensa](https://deivi.ch/defensa).
+
+### Compilación de animaciones
+
+- Requisitos: [manim](https://docs.manim.community/en/stable/installation.html)
+
+```shell
+manim -pqm scene.py NombreAnimacion
+```
+
+## Licencia
+
+- El código de B6+ (`b6plus.js`, `simple.css`, `Overview.html`) se comparte bajo la licencia pública del W3C.
+- El resto de ficheros de código se comparten, a elección de la persona usuaria, bajo los términos de la [Mozilla Public License, versión 2](https://www.mozilla.org/en-US/MPL/2.0/) o la [GNU Affero General Public License, versión 3](https://www.gnu.org/licenses/agpl-3.0.en.html).
---
+++ scene.py
@@ -0,0 +1,92 @@
+from manim import *
+
+class CreateNet(Scene):
+    def layer_objects(self, n_units=1, left=0, color=RED):
+        nodes1 = [Circle(.5).set_color(color) for _ in range(n_units)]
+        nodes = VGroup(*nodes1)
+        middle = (n_units - 1)/2
+        for i, n in enumerate(nodes1):
+            n.shift(DOWN * (i - middle) * 1.2)
+            n.shift(LEFT * 2 * left)
+        self.animation_queue.append(Create(nodes, lag_ratio=.5))
+
+        return nodes1
+
+    def play_queue(self):
+        self.play(*self.animation_queue)
+        self.clear_queue()
+
+    def clear_queue(self):
+        self.animation_queue.clear()
+
+    # def create_layer(self, n_units=1, left=0, color=RED):
+    #     nodes = self.layer_objects(n_units, left, color)
+    #     self.play(*[Create(n) for n in nodes])
+    #     return nodes
+
+    def connect_layers(self, l1, l2):
+        lines = VGroup(*[Line(start=n.get_right(), end=m.get_left()).set_color(BLACK) for m in l2 for n in l1])
+        self.animation_queue.append(Create(lines, lag_ratio=.2))
+        return lines
+
+    def transform_many(self, l_from, l_to):
+        for a, b in zip(l_from, l_to):
+            self.animation_queue.append(Transform(a, b))
+
+    def construct(self):
+        self.animation_queue = []
+        # set background to white (remember to set stuff to black since there are some default whites)
+        self.camera.background_color = WHITE
+
+        # create "variables" represented as columns in a table
+        table = [Rectangle(width=1.0, height=2.0, grid_xstep=1.0, grid_ystep=0.5).set_color(BLACK) for _ in range(5)]
+        for i, n in enumerate(table):
+            n.shift(LEFT * (i - 2))
+        
+        self.play(Create(table[2]))
+        self.play(Create(table[1]), Create(table[3]))
+        self.play(Create(table[0]), Create(table[4]))
+        
+        # transform those variables into a set of 5 nodes, the input layer
+        layer1 = self.layer_objects(5, 1, GREEN)
+        self.clear_queue()
+        self.wait(1.)
+        self.transform_many(table, layer1)
+        self.play_queue()
+        self.add(*layer1)
+
+        # "copy" those nodes onto the final output layer
+        self.wait(0.5)
+        layer3 = table # use the nodes that were just transformed as last layer
+        self.play(*[n.animate.shift(4 * RIGHT) for n in layer3])
+        
+        # create a middle "encoding" layer
+        self.wait(0.5)
+        layer2 = self.layer_objects(2, 0, PURPLE)
+        self.play_queue()
+
+        # connect layers to create a neural network
+        l12c = self.connect_layers(layer1, layer2)
+        self.play_queue()
+        l23c = self.connect_layers(layer2, layer3)
+        self.play_queue()
+
+        # fade out connections
+        self.play(*[l.animate.fade(1) for l in l12c], *[l.animate.fade(1) for l in l23c])
+
+        # separate input and output
+        self.play(*[n.animate.shift(2*LEFT) for n in layer1], *[n.animate.shift(2*RIGHT) for n in layer3])
+        
+        # create more hidden layers
+        layer4 = self.layer_objects(3, 1)
+        layer5 = self.layer_objects(3, -1)
+        self.play_queue()
+
+        self.connect_layers(layer1, layer4)
+        self.connect_layers(layer4, layer2)
+        self.connect_layers(layer2, layer5)
+        self.connect_layers(layer5, layer3)
+        self.play_queue()
+
+
+
